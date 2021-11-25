3068b3a495af3ac8d25a3514b4d1b71774758b1a
Douglas <douglaskerins@gmail.com>
2021-11-25 14:22:55 +0000

    refactoring for readability

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -15,7 +15,7 @@ class Shop {
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
           if (this.items[i].quality > 0) {
-            this.items[i].quality = this.items[i].quality - 1;
+            this.items[i].quality --;
           }
         } else {
           if (this.items[i].quality < 50) {
@@ -23,12 +23,12 @@ class Shop {
             if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
               if (this.items[i].sellIn < 11) {
                 if (this.items[i].quality < 50) {
-                  this.items[i].quality = this.items[i].quality + 1;
+                  this.items[i].quality ++;
                 }
               }
               if (this.items[i].sellIn < 6) {
                 if (this.items[i].quality < 50) {
-                  this.items[i].quality = this.items[i].quality + 1;
+                  this.items[i].quality ++;
                 }
               }
             }
@@ -40,15 +40,15 @@ class Shop {
             if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
               if (this.items[i].quality > 0) {
                 if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                  this.items[i].quality = this.items[i].quality - 1;
+                  this.items[i].quality --;
                 }
               }
             } else {
-              this.items[i].quality = this.items[i].quality - this.items[i].quality;
+              this.items[i].quality = 0;
             }
           } else {
             if (this.items[i].quality < 50) {
-              this.items[i].quality = this.items[i].quality + 1;
+              this.items[i].quality ++;
             }
           }
         }
