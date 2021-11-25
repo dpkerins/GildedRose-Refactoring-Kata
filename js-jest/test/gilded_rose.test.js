01644382328a6ea45dc1f28247f8ae038f51dcfd
Douglas <douglaskerins@gmail.com>
2021-11-25 11:00:44 +0000

    adding tests for regularly depreciating items

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -6,4 +6,27 @@ describe("Gilded Rose", function() {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("foo");
   });
+
+  describe("Regular Items", () => {
+    it("should decrease quality by one", function() {
+      const gildedRose = new Shop([new Item("cool item", 10, 20)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(19);
+    });
+    it("should decrease sellIn by one", () => {
+      const gildedRose = new Shop([new Item("cool item", 10, 20)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(9);
+    });
+    it("should decrease quality by two", () => {
+      const gildedRose = new Shop([new Item("cool item", 0, 20)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(18);
+    })
+    it("should not decrease quality past zero", () => {
+      const gildedRose = new Shop([new Item("cool item", 5, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(0);
+    })
+  })  
 });
