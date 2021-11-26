d13aadb5637714fc0f5ae174fbc162b93c23484c
Douglas <douglaskerins@gmail.com>
2021-11-26 13:49:43 +0000

    refactoring and using linter

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -16,25 +16,29 @@ class Shop {
       const isAgedBrie = item.name == "Aged Brie";
       const isBackstagePass = item.name == 'Backstage passes to a TAFKAL80ETC concert';
       const isConjured = item.name == "Conjured Mana Cake";
-      if (isAgedBrie) { item.quality = this.calcAgedBrieQuality(item) };
-      if (isBackstagePass) { item.quality = this.calcBackstageQuality(item) };
-      if (!isAgedBrie && !isBackstagePass && !isSulfuras && !isConjured) { item.quality = this.calcRegularItemQuality(item) };
-      if (isConjured) { item.quality = this.calcConjuredQuality(item) };
+      if (isAgedBrie) { item.quality = this.calcAgedBrieQuality(item) }
+      if (isBackstagePass) { item.quality = this.calcBackstageQuality(item) }
+      if (!isAgedBrie && !isBackstagePass && !isSulfuras && !isConjured) { item.quality = this.calcRegularItemQuality(item) }
+      if (isConjured) { item.quality = this.calcConjuredQuality(item) }
       
-
       const isQualityOver50 = item.quality > 50;
       const isQualityUnder0 = item.quality < 0;
-      if (isQualityOver50) { item.quality = 50 };
-      if (isQualityUnder0) { item.quality = 0 };
-      if (!isSulfuras) { item.sellIn-- };
+      if (isQualityOver50) { item.quality = 50 }
+      if (isQualityUnder0) { item.quality = 0 }
+      this.getNewSellIn(item);
     })
     return this.items;
+  }
+
+  getNewSellIn(item) {
+    const isSulfuras = item.name == "Sulfuras, Hand of Ragnaros";
+    if (!isSulfuras) { item.sellIn-- }
   }
 
   calcRegularItemQuality(item) {
     const isPastSellIn = item.sellIn < 1;
 
-    if (isPastSellIn) { return item.quality -= 2 };
+    if (isPastSellIn) { return item.quality -= 2 }
     return item.quality -= 1;
   }
 
@@ -43,23 +47,23 @@ class Shop {
     const isFiveDaysFromSellin = item.sellIn < 6;
     const isTenDaysFromSellin = item.sellIn < 11;
 
-    if (isPastSellIn) { return 0 };
-    if (isFiveDaysFromSellin) { return item.quality += 3 };
-    if (isTenDaysFromSellin) { return item.quality += 2 };
+    if (isPastSellIn) { return 0 }
+    if (isFiveDaysFromSellin) { return item.quality += 3 }
+    if (isTenDaysFromSellin) { return item.quality += 2 }
     return item.quality += 1;
   }
 
   calcAgedBrieQuality(item) {
     const isPastSellIn = item.sellIn < 1;
 
-    if (isPastSellIn) { return item.quality += 2 };
+    if (isPastSellIn) { return item.quality += 2 }
     return item.quality += 1;
   }
 
   calcConjuredQuality(item) {
     const isPastSellIn = item.sellIn < 1;
 
-    if (isPastSellIn) { return item.quality -= 4 };
+    if (isPastSellIn) { return item.quality -= 4 }
     return item.quality -= 2;
   }
 }
