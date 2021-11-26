8b906dcd255daed0cfbad84282b0fb88e85ee386
Douglas <douglaskerins@gmail.com>
2021-11-26 08:54:33 +0000

    removing remaining nesting

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -17,42 +17,40 @@ class Shop {
       const isBackstagePass = item.name == 'Backstage passes to a TAFKAL80ETC concert';
       const isPastSellIn = item.sellIn < 1;
       const isConjured = item.name == "Conjured Mana Cake";
-      if (!isSulfuras) {
-        if (isAgedBrie || isBackstagePass) {
-          item.quality ++;
-        }
-        if (isAgedBrie && isPastSellIn) {
-          item.quality ++;
-        }
-        if (isBackstagePass && item.sellIn < 11) {
-          item.quality++;
-        }
-        if (isBackstagePass && item.sellIn < 6) {
-          item.quality++;
-        }
-        if (isBackstagePass && isPastSellIn) {
-          item.quality = 0;
-        }
-        if (!isAgedBrie && !isBackstagePass) {
-          item.quality --;
-        }
-        if (isConjured) {
-          item.quality --;
-        }
-        if (!isAgedBrie && !isBackstagePass && isPastSellIn) {
-          item.quality --;
-        }
-        if (isConjured && isPastSellIn) {
-          item.quality --;
-        }
-        if (item.quality > 50) {
-          item.quality = 50;
-        }
-        if (item.quality < 0) {
-          item.quality = 0;
-        }
-        item.sellIn --;
+      if (isAgedBrie || isBackstagePass) {
+        item.quality ++;
       }
+      if (isAgedBrie && isPastSellIn) {
+        item.quality ++;
+      }
+      if (isBackstagePass && item.sellIn < 11) {
+        item.quality++;
+      }
+      if (isBackstagePass && item.sellIn < 6) {
+        item.quality++;
+      }
+      if (isBackstagePass && isPastSellIn) {
+        item.quality = 0;
+      }
+      if (!isAgedBrie && !isBackstagePass && !isSulfuras) {
+        item.quality --;
+      }
+      if (isConjured) {
+        item.quality --;
+      }
+      if (!isAgedBrie && !isBackstagePass && !isSulfuras && isPastSellIn) {
+        item.quality --;
+      }
+      if (isConjured && isPastSellIn) {
+        item.quality --;
+      }
+      if (item.quality > 50) {
+        item.quality = 50;
+      }
+      if (item.quality < 0) {
+        item.quality = 0;
+      }
+      if (!isSulfuras) { item.sellIn-- };
     })
     return this.items;
   }
